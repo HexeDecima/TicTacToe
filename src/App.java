8c1c477e28fc0002ef036fe11e216cfff0cf24c1
Hexe Decima <150820220+HexeDecima@users.noreply.github.com>
2024-11-06 19:43:48 +0100

    Update App.java
    Initial upload caused wrong file contents.

--- src/App.java
+++ src/App.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
 public class App {
+    public App() {
+    }
+
+    public static void main(String[] args) throws Exception {
+        new TicTacToe();
+    }
 }
